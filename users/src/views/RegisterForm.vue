693ca564cd8134cd0208d9426c6cabda792fa60a
Leonardo Schunck Rainha <schunckrainhaleonardo@gmail.com>
2023-09-12 17:35:22 -0300

    feat: msgsError and msgCorrect

--- users/src/views/RegisterForm.vue
+++ users/src/views/RegisterForm.vue
@@ -2,21 +2,90 @@
   <div class="container">
     <h2 class="display-2 text-center">Registro de usuário</h2>
     <label for="name" class="form-label">Nome:</label>
-    <input type="text" name="name" class="form-control mb-3">
+    <input
+      type="text"
+      name="name"
+      class="form-control mb-3"
+      v-model="name"
+      placeholder="Digite o seu nome"
+    />
     <label for="email" class="form-label">E-mail:</label>
-    <input type="email" name="email" class="form-control mb-3">
+    <input
+      type="email"
+      name="email"
+      class="form-control mb-3"
+      v-model="email"
+      placeholder="Digite o seu E-mail"
+    />
     <label for="password" class="form-label">Senha:</label>
-    <input type="text" name="password" class="form-control mb-3">
-    <button class="btn btn-success w-100 btn-lg">Cadastrar</button>
+    <input
+      type="password"
+      name="password"
+      class="form-control mb-3"
+      v-model="password"
+      placeholder="Digite sua senha"
+    />
+    <div
+      v-if="isCorrect"
+      class="alert alert-success d-flex align-items-center"
+      role="alert"
+    >
+      <div>Usuário registrado com sucesso</div>
+    </div>
+    <div v-if="isError" class="alert alert-danger d-flex align-items-center" role="alert">
+      <div>{{msgErr}}</div>
+    </div>
+    <button class="btn btn-success w-100 btn-lg" @click="register">
+      Cadastrar
+    </button>
   </div>
 </template>
 
 <script>
-export default {
+import axios from "axios";
 
-}
+export default {
+  data() {
+    return {
+      name: "",
+      email: "",
+      password: "",
+      isCorrect: false,
+      msgErr: '',
+      isError: false
+    };
+  },
+  methods: {
+    register() {
+      let userRegister = {
+        name: this.name,
+        email: this.email,
+        password: this.password,
+      };
+
+      axios
+        .post("http://localhost:8686/user", userRegister)
+        .then((res) => {
+          console.log(res);
+          this.name = "";
+          this.email = "";
+          this.password = "";
+          this.isCorrect = true;
+          this.isError = false
+          setTimeout(() => {
+            this.isCorrect = false;
+          }, 5000);
+        })
+        .catch((err) => {
+          let msgError = err.response.data.err;
+          this.msgErr = msgError
+          this.isError = true
+          console.log(msgError);
+        });
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
--- usersAPI/index.js
+++ usersAPI/index.js
@@ -2,9 +2,12 @@ const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
 const router = require('./routes/routes')
+const cors = require('cors')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+app.use(cors())
 
 app.use('/', router)
 
